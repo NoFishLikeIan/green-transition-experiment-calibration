0db012a1d9ec14412df9326fd93ac3d62aa70d20
NoFishLikeIan <andreatitton96@gmail.com>
2026-08-27 11:53:22 +0200

    🐛 wrong minimal

--- maximal-penalty.jl
+++ maximal-penalty.jl
@@ -4,22 +4,19 @@ import LaTeXStrings: @L_str
 CairoMakie.activate!()
 
 function P₁(γ, T)
-    r = 30γ / T
-    return 500 * r / 3
+    return 5000γ / T
 end
 
 function P₂(γ, T)
-    r = 30γ / T
-    y = (4T + 1) / (2T + 1)
-    return 500 * (1 - r * y / 3)
+    return 500 * (1 - 20γ - 10γ / T)
 end
 
 
 begin
     T = 5
-    γgrid = range(0, T / 30, 101)
+    γgrid = range(0, inv(20 - 20 / (2T + 1) + 30 / T), 101)
     figure = CairoMakie.Figure()
-    axis = CairoMakie.Axis(figure[1, 1], xlabel = L"\gamma", ylabel = "Points", title = L"Fines with $T=%$T$", limits = ((0, T / 30), (0., 500.)))
+    axis = CairoMakie.Axis(figure[1, 1], xlabel = L"\gamma", ylabel = "Points", title = L"Fines with $T=%$T$", limits = ((0, last(γgrid)), (0., 500.)))
     P₁fig = CairoMakie.lines!(axis, γgrid, γ -> P₁(γ, T))
     P₂fig = CairoMakie.lines!(axis, γgrid, γ -> P₂(γ, T))
 
@@ -30,7 +27,8 @@ begin
     figure
 end
 
-γ = 33 // 200
+# Preferred 
+γ = 3 // 100
 T = 5//1
 P₁(γ, T)
 P₂(γ, T)
